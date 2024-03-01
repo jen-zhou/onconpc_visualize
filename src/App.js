332b0f9ee470e610667b5bfc178125db2975f017
Jennifer <jenz33@mit.edu>
2024-03-01 15:30:56 -0500

    added nav bar

--- src/App.js
+++ src/App.js
@@ -1,20 +1,21 @@
 import logo from "./logo.svg";
 import "./App.css";
+import Navbar from "./Navbar";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-
+      <Navbar />
+      <div style={{ flexGrow: 1 }}>
         <iframe
           title="onconpc"
+          className="gradio"
           src="https://jenniferzhou33-onconpc-visualization.hf.space"
           frameborder="0"
           width="850"
           height="450"
         ></iframe>
-
-      </header>
+      </div>
     </div>
   );
 }
---
+++ src/Navbar.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <nav
+      style={{
+        display: "flex",
+        justifyContent: "space-between",
+        padding: "1rem",
+        background: "#bde3f6",
+        color: "black",
+      }}
+    >
+      <div>
+        <span>OncoNPC</span>
+      </div>
+      <div>
+        <a href="/" style={{ color: "black", marginRight: "1rem" }}>
+          Tutorial
+        </a>
+        <a href="/about" style={{ color: "black", marginRight: "1rem" }}>
+          Prediction Tool
+        </a>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
