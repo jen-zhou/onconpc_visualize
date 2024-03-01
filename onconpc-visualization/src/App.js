be0445f55fb738d3e1b984a1188e86d40119e13e
Jennifer <jenz33@mit.edu>
2024-03-01 12:38:56 -0500

    react app start

---
+++ onconpc-visualization/src/App.js
@@ -0,0 +1,32 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <iframe
+          title="onconpc"
+          src="https://jenniferzhou33-onconpc-visualization.hf.space"
+          frameborder="0"
+          width="850"
+          height="450"
+        ></iframe>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
